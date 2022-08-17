16b10d989990f1fd0d683afacd5e4b4977b76a55
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2022-08-17 15:57:59 +0300

    Created project

---
+++ NoteBook/NoteBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteBook
+//
+//  Created by Ваня Науменко on 17.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
